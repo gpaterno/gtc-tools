1d879c178a6bc5f2c55315bf1f1bca7003f5e5da
Giuseppe Paterno <gpaterno@gpaterno.com>
2012-11-16 23:34:02 +0100

    First skeleton

--- gtc-update.py
+++ gtc-update.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 #
+# GTC Updater
+# (c) 2012 Giuseppe "Gippa" Paterno' (gpaterno@gpaterno.com)
+#
 # * Load config from /etc/liveimg.conf
 # * Check if root (has to be run as root via sudo)
 # * Check connectivity and reachability of the server
@@ -14,6 +17,32 @@
 # * Done :)
 #
 
+import os
+import sys
+from ctypes import *
+
+MS_REMOUNT = 32
+MS_RDONLY  = 1
+LINUX_LIBC = "libc.so.6"
+MOUNTPOINT = "/cdrom"
 
 if __name__ == '__main__':
-	print "Ciao"
+	libc = cdll.LoadLibrary(LINUX_LIBC)
+
+	## Let's remount the filesystem in read-write
+	if libc.mount(None, "/cdrom", MS_REMOUNT, None) != 0:
+		print "Cannot remount filesystem in read-write, mount operation failed"
+		exit(1)
+
+	# Check if the filesystem is in read-write
+	# mount operation returns 0 also if it's on a CD, so let's check
+	# if we are in read-write
+	if os.statvfs("/cdrom")[8] & MS_RDONLY:
+		print "You're running from a CD (or we're having problem mounting)"
+		exit(1)
+
+
+	## Reset the filesystem in read-only
+	if libc.mount(None, "/cdrom", MS_REMOUNT = MS_RDONLY, None) != 0:
+		print "Cannot remount filesystem in read-only, mount operation failed"
+		exit(1)
