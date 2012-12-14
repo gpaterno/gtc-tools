1ff70b0c74e8fca199cf9c6b34a35e8b02d492b6
sheva <user@user-1011PX.(none)>
2012-12-14 19:02:37 +0100

    mergiato gtc-update e gtc-update-qt

--- gtc-update.py
+++ gtc-update.py
@@ -323,8 +323,7 @@ logging.basicConfig(format=logfmt, level=logging.DEBUG)
 logger = logging.getLogger("gtc-updater")
 
 
-if __name__ == '__main__':
-
+def cmdline()
 	## Loading config
 	getconf()
 	## Welcome msg and warning
@@ -349,3 +348,86 @@ if __name__ == '__main__':
 
 
 
+
+from PyQt4 import QtCore, QtGui
+from mainwindow import *
+ 
+class MainWindow (object):
+	 
+	def __init__(self):
+		self.app = QtGui.QApplication([])
+		self.Dialog = QtGui.QMainWindow()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self.Dialog)
+		self.connections()
+
+	 
+	def connections(self):
+		#BUTTON CONNECTIONS
+		self.app.connect(self.ui.ckupdate, QtCore.SIGNAL("clicked()"), self.ckupdates)
+		 
+		 
+	def run(self):
+		self.Dialog.show()
+		self.app.exec_()
+		 
+	def clear(self):
+		self.ui.textEdit.clear()
+		self.ui.htmlEdit.clear()
+	 
+	def ckupdates(self):
+		rval = getconf() 
+		if rval != 1:
+			self.ui.lbloutput.setText("Impossibile trovare il file di configurazione\n")
+			self.ui.lbloutput.setText("Controlla l'esistenza di global.ini nella directory /etc/gtc/ \n")
+			return(0)
+
+		rval = ckroot()
+		if rval == 0:
+			self.ui.lbloutput.setText("Devi essere amministratore per eseguire l'aggiornamento del GTC")
+			return(0)
+
+		rval = 	mount_device()
+		if rval == 0:
+			self.ui.lbloutput.setText("Errore durante il mount del device")
+			return(0)
+					
+		rval = create_tmpdir()
+		if rval == 0:
+			self.ui.lbloutput.setText("Errore nella creazione della directory temporanea")
+			return(0)
+
+		rval = download_config()
+		if rval == 0:
+			self.ui.lbloutput.setText("Errore nella creazione della directory temporanea")
+			return(0)
+
+
+		rval = ckrelease()
+		if rval == 0:
+			self.ui.lbloutput.setText("Errore")
+			return (0)
+		elif rval == 1:
+			self.ui.lbloutput.setText("La release e' aggiornata")
+			return(0)
+		elif rval == 2:
+			self.ui.lbloutput.setText("Devi aggiornare!")
+			# abilitare il tasto di avvio aggiornamento.
+			
+		 
+if __name__ == "__main__":
+	# da aggiungere controllo cmdline o qtinterface 
+	qtinterface()
+
+
+def qtinterface():
+	import logging
+
+	## Setup logging first
+	logfmt = " %(levelname)s: %(message)s"
+	logging.basicConfig(format=logfmt, level=logging.DEBUG)
+	global logger
+	logger = logging.getLogger("gtc-updater")
+
+	window = MainWindow()
+	window.run()
